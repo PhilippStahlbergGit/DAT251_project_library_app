a4986f726980d0c8e2d73de22a918ec28545d2d3
Runar Røssevold <126164337+ranuru@users.noreply.github.com>
2026-02-25 15:02:41 +0100

    Refactor User addition logic in UserController and UserService to use UserRequest DTO directly

--- src/main/java/app/main/LibraryApp/service/UserService.java
+++ src/main/java/app/main/LibraryApp/service/UserService.java
@@ -6,19 +6,24 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import app.main.LibraryApp.domain.User;
+import app.main.LibraryApp.domain.dto.UserRequest;
 
 @Service
 public class UserService {
 
     List<User> users;
-    
+
     public UserService(List<User> users) {
         this.users = new ArrayList<>(users);
     }
 
-    public User addUser(User user) {
-        this.users.add(user);
-        return user;
+    public User addUser(UserRequest user) {
+        User newUser = new User();
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(user.getPassword());
+        this.users.add(newUser);
+        return newUser;
     }
 
     public List<User> getAllUsers() {
@@ -28,6 +33,5 @@ public class UserService {
     public boolean deleteUser(Long id) {
         return this.users.removeIf(user -> user.getId().equals(id));
     }
-
 
 }
