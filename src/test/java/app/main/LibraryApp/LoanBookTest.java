71dfea4d6e343f42cacd51fc409bf9aec58047d0
Elend.Kasem <kasem.elend@gmail.com>
2026-02-25 18:44:56 +0100

    cleaned up some code

--- src/test/java/app/main/LibraryApp/LoanBookTest.java
+++ src/test/java/app/main/LibraryApp/LoanBookTest.java
@@ -4,24 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import app.main.LibraryApp.domain.BookCopy;
 import app.main.LibraryApp.domain.Loan;
-import app.main.LibraryApp.domain.enums.AvailabilityStatus;
 import app.main.LibraryApp.domain.enums.LoanStatus;
 import app.main.LibraryApp.service.LoanService;
 
 class LoanBookTest {
 
     @Test
-    public void testCreateLoan() {
+    void testCreateLoan() {
         // test for creating a loan
-        BookCopy bookCopy = new BookCopy();
-        bookCopy.setId(1L);
-        bookCopy.setAvailabilityStatus(AvailabilityStatus.AVAILABLE);   
         
         LoanService loanService = new LoanService();
         Loan loan = loanService.createLoan("John Doe", "The Great Gatsby");
-
 
         assertEquals("John Doe", loan.getBorrowerName());
         assertEquals("The Great Gatsby", loan.getBookTitle());
