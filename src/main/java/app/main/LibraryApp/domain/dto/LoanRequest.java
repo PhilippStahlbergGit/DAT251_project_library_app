f532bd3676ee591470e4c64238a9215aaf9f637d
Elend.Kasem <kasem.elend@gmail.com>
2026-02-25 16:33:41 +0100

    implemented createLoan method in Loancontroller

--- src/main/java/app/main/LibraryApp/domain/dto/LoanRequest.java
+++ src/main/java/app/main/LibraryApp/domain/dto/LoanRequest.java
@@ -1,10 +1,12 @@
 package app.main.LibraryApp.domain.dto;
 
+import app.main.LibraryApp.domain.BookCopy;
 import lombok.Data;
 
 @Data
 public class LoanRequest {
     
+    private BookCopy bookCopy;
     private String loanComment;
     private String strBorrower;
 }
